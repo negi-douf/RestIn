c6874b5226d403b7673fe75fe805322dff1e6bd8
KouyaHirata <atarih.ayuok@gmail.com>
2017-07-05 16:54:31 +0000

    complete print all tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,23 @@
 class TweetsController < ApplicationController
   
   def index
+    
+    # 一覧表示用
     @tweets = Tweet.all
+    
+    # form_for用
+    # 一覧と同じ画面で投稿も実現したいから
+    # この時点では保存しない
+    # ムダに作ってる感じがするので、
+    # もっといい方法が見つかれば改善したい
     @tweet = Tweet.new()
+    
   end
   
   
   def create
+    
+    # form_for で取得した内容から作成
     @tweet = Tweet.new(tweets_params)
     
     if @tweet.save
@@ -14,13 +25,19 @@ class TweetsController < ApplicationController
       redirect_to root_path
     else
       # flash message
-      
     end
     
   end
   
   
+  def edit
+    
+  end
   
+  
+  def destroy
+    
+  end
   
   
   
@@ -29,6 +46,9 @@ class TweetsController < ApplicationController
   private
   #######
   
+  # strong parameter
+  # tweet に関するパラメータのみを許可し保持する
+  # 使用は必須
   def tweets_params
     params.require(:tweet).permit(:content)
   end
