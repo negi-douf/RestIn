5b81680dfd74a1e5bf36bc324725155857f7f75b
KouyaHirata <atarih.ayuok@gmail.com>
2017-07-05 16:29:17 +0000

    submit complete

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,38 @@
 class TweetsController < ApplicationController
+  
   def index
+    @tweets = Tweet.all
+    @tweet = Tweet.new()
   end
+  
+  
+  def create
+    @tweet = Tweet.new(tweets_params)
+    
+    if @tweet.save
+      # flash message
+      redirect_to root_path
+    else
+      # flash message
+      
+    end
+    
+  end
+  
+  
+  
+  
+  
+  
+  
+  #######
+  private
+  #######
+  
+  def tweets_params
+    params.require(:tweet).permit(:content)
+  end
+  
+  
+  
 end
