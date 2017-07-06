769dc42fdf86a147eb981a5ff029f01c0b093d4e
KouyaHirata <atarih.ayuok@gmail.com>
2017-07-06 15:52:19 +0000

    add edit and sort reverse.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   def index
     
     # 一覧表示用
-    @tweets = Tweet.all
+    @tweets = Tweet.all.sort.reverse
     
     # form_for用
     # 一覧と同じ画面で投稿も実現したいから
@@ -30,6 +30,21 @@ class TweetsController < ApplicationController
   
   
   def edit
+    # id を使って対象を特定
+    # 取得するパラメータが一つの場合は
+    # ストロングパラメータはいらない
+    @tweet = Tweet.find_by(id: params[:id])
+  end
+  
+  
+  def update
+    @tweet = Tweet.find_by(id: params[:id])
+    
+    if @tweet.update(tweets_params)
+      redirect_to root_path, notice: "ツイートを編集しました！"
+    else
+      redirect_to edit_tweet_path, notice: "ツイートの編集に失敗しました"
+    end
     
   end
   
